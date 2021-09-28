551c9a90e69337e8f0c5abd808a1679be2722941
Ssmartcode <razvan.anca.andrei@gmail.com>
2021-09-28 14:31:05 +0300

    create a table based on a dummy matrix

---
+++ src/components/ChessTable/ChessTable.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import initialMatrix from "./matrix.js";
+import { v4 } from "uuid";
+import "./ChessTable.css";
+
+const ChessTable = () => {
+  const [matrix, setMatrix] = useState(initialMatrix);
+  return (
+    <div className="chess-table">
+      {matrix.map((row, rowIndex) => {
+        return row.map((piece, pieceIndex) => {
+          {
+            /* if the index of the moving piece has the same parity as the index of the line then the tale is light else it is dark */
+          }
+          const parity = pieceIndex % 2 === rowIndex % 2;
+          return (
+            <div
+              key={v4()}
+              className={`chess-square--${parity ? "light" : "dark"}`}
+            ></div>
+          );
+        });
+      })}
+    </div>
+  );
+};
+
+export default ChessTable;
